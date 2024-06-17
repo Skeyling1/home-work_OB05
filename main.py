6a7347bf046839f564ef85ce3d0fc6542ab5f733
Skeyling1 <skeyling1@gmail.com>
2024-06-17 22:02:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 # змейка
 
 import pygame
+pygame.init()
 
-#создать экран 800*600
+scee_size = (800, 600)
+screen = pygame.display.set_mode(scee_size)
+pygame.display.set_caption("Snake Game")
+
 
 running = True
-
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
     # событие
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+
+
+pygame.quit()
 

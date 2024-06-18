671383a3a834947733af485e21837a441e0b75c4
Skeyling1 <skeyling1@gmail.com>
2024-06-18 17:26:31 +0300

    swq

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# змейка
+# игра змейка
 
 import pygame
 pygame.init()
@@ -7,6 +7,20 @@ scee_size = (800, 600)
 screen = pygame.display.set_mode(scee_size)
 pygame.display.set_caption("Snake Game")
 
+image = pygame.image.load("tail-1.png")
+x = 50
+y = 50
+position = (x, y)
+
+
+class Head:
+    pass
+
+class Tail:
+    pass
+
+class Eatables:
+    pass
 
 running = True
 while running:
@@ -14,8 +28,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # событие
     screen.fill((0, 0, 0))
+    screen.blit(image, position)
+    x += 1
+    y += 1
+    position = (x, y)
+
+
+
+
+
     pygame.display.flip()
 
 

63bd4482da689959c72b2124b7bd22f71ea3ffd5
Skeyling1 <skeyling1@gmail.com>
2024-06-18 17:40:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ pygame.display.set_caption("Snake Game")
 image = pygame.image.load("tail-1.png")
 x = 50
 y = 50
+direction_x = 0
+direction_y = 0
 position = (x, y)
 
 
@@ -30,8 +32,22 @@ while running:
 
     screen.fill((0, 0, 0))
     screen.blit(image, position)
-    x += 1
-    y += 1
+    pygame.time.delay(10)
+
+
+    keys = pygame.key.get_pressed()
+
+    x += direction_x
+    y += direction_y
+    if keys[pygame.K_RIGHT]:
+        direction_x = 1
+    elif keys[pygame.K_LEFT]:
+        direction_x = -1
+    elif keys[pygame.K_UP]:
+        direction_y = -1
+    elif keys[pygame.K_DOWN]:
+        direction_y = 1
+
     position = (x, y)
 
 
